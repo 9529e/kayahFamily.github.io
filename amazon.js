745b1972eb71e24a9da42a978011b3b3b91f02fa
9529e <102638086+9529e@users.noreply.github.com>
2023-05-03 05:45:11 -0400

    Add files via upload

---
+++ amazon.js
@@ -0,0 +1,29 @@
+function button1() {
+    window.location.href='https://www.youtube.com/watch?v=RroDdybvu5s';
+}
+
+function button2() {
+
+}
+
+function button3() {
+
+}
+
+function button4() {
+
+}
+
+function button5() {
+
+}
+
+function button6() {
+
+}
+
+function button7() {
+
+}
+
+
